8a9d60a152b704268ebdabc46ef2e383675ee9a8
Kyle Kastner <kastnerkyle@gmail.com>
2018-02-27 00:02:03 -0500

    added pe

--- tfbldr/datasets/datasets.py
+++ tfbldr/datasets/datasets.py
@@ -15,6 +15,65 @@ import fnmatch
 from scipy import linalg
 from functools import wraps
 import exceptions
+import subprocess
+import shutil
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+        shutil.copystat(src, dst)
+    lst = os.listdir(src)
+    if ignore:
+        excl = ignore(src, lst)
+        lst = [x for x in lst if x not in excl]
+    for item in lst:
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if symlinks and os.path.islink(s):
+            if os.path.lexists(d):
+                os.remove(d)
+            os.symlink(os.readlink(s), d)
+            try:
+                st = os.lstat(s)
+                mode = stat.S_IMODE(st.st_mode)
+                os.lchmod(d, mode)
+            except:
+                pass  # lchmod not available
+        elif os.path.isdir(s):
+            copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+def pwrap(args, shell=False):
+    p = subprocess.Popen(args, shell=shell, stdout=subprocess.PIPE,
+                         stdin=subprocess.PIPE, stderr=subprocess.PIPE,
+                         universal_newlines=True)
+    return p
+
+# Print output
+# http://stackoverflow.com/questions/4417546/constantly-print-subprocess-output-while-process-is-running
+def execute(cmd, shell=False):
+    popen = pwrap(cmd, shell=shell)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line
+
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+
+def pe(cmd, shell=False, verbose=True):
+    """
+    Print and execute command on system
+    """
+    all_lines = []
+    for line in execute(cmd, shell=shell):
+        if verbose:
+            print(line, end="")
+        all_lines.append(line.strip())
+    return all_lines
 
 
 def make_mask(arr):
