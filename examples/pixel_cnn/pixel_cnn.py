3a77b3422260b45e1e239e2946732be866f2147d
Kyle Kastner <kastnerkyle@gmail.com>
2018-04-30 18:04:39 -0400

    first pixcnn

--- examples/pixel_cnn/pixel_cnn.py
+++ examples/pixel_cnn/pixel_cnn.py
@@ -1,6 +1,6 @@
 from tfbldr.datasets import fetch_mnist
 from tfbldr.nodes import GatedMaskedConv2d
-from tfbldr.nodes import ConvTranspose2d
+from tfbldr.nodes import Conv2d
 from tfbldr.nodes import VqEmbedding
 from tfbldr.nodes import BatchNorm2d
 from tfbldr.nodes import ReLU
@@ -28,8 +28,9 @@ random_state = np.random.RandomState(1999)
 kernel_size0 = (7, 7)
 kernel_size1 = (3, 3)
 n_channels = 64
+n_layers = 15
 
-def create_pixel_cnn(inp, bn_flag):
+def create_pixel_cnn(inp):
     l1_v, l1_h = GatedMaskedConv2d([inp], [1], [inp], [1],
                                    n_channels,
                                    residual=False,
@@ -38,7 +39,7 @@ def create_pixel_cnn(inp, bn_flag):
                                    random_state=random_state)
     o_v = l1_v
     o_h = l1_h
-    for i in range(14):
+    for i in range(n_layers - 1):
         t_v, t_h = GatedMaskedConv2d([o_v], [n_channels], [o_h], [n_channels],
                                      n_channels,
                                      kernel_size=kernel_size1, name="pcnn{}".format(i + 1),
@@ -46,68 +47,38 @@ def create_pixel_cnn(inp, bn_flag):
                                      random_state=random_state)
         o_v = t_v
         o_h = t_h
-    from IPython import embed; embed(); raise ValueError()
-    """
-    l3 = Conv2d([r_l2], [l_dims[1][0]], l_dims[2][0], kernel_size=l_dims[2][1:3], name="enc3",
-                random_state=random_state)
-    bn_l3 = BatchNorm2d(l3, bn_flag, name="bn_enc3")
-    """
-    return bn_l3
+
+    cleanup = Conv2d([o_h], [n_channels], n_channels, kernel_size=(1, 1),
+                     name="conv_c",
+                     random_state=random_state)
+    r_p = ReLU(cleanup)
+    out = Conv2d([r_p], [n_channels], 1, kernel_size=(1, 1),
+                 name="conv_o",
+                 random_state=random_state)
+    s_out = Sigmoid(out)
+    return s_out
 
 
 def create_graph():
     graph = tf.Graph()
     with graph.as_default():
         images = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
-        bn_flag = tf.placeholder_with_default(tf.zeros(shape=[]), shape=[])
-        x_tilde = create_pixel_cnn(images, bn_flag)
-        raise ValueError()
-        rec_loss = tf.reduce_mean(BernoulliCrossEntropyCost(x_tilde, images))
-        vq_loss = tf.reduce_mean(tf.square(tf.stop_gradient(z_e_x) - z_q_x))
-        commit_loss = tf.reduce_mean(tf.square(z_e_x - tf.stop_gradient(z_q_x)))
-        #rec_loss = tf.reduce_mean(tf.reduce_sum(BernoulliCrossEntropyCost(x_tilde, images), axis=[1, 2]))
-        #vq_loss = tf.reduce_mean(tf.reduce_sum(tf.square(tf.stop_gradient(z_e_x) - z_q_x), axis=[1, 2, 3]))
-        #commit_loss = tf.reduce_mean(tf.reduce_sum(tf.square(z_e_x - tf.stop_gradient(z_q_x)), axis=[1, 2, 3]))
-        beta = 0.25
-        loss = rec_loss + vq_loss + beta * commit_loss
+        x_tilde = create_pixel_cnn(images)
+        loss = tf.reduce_mean(BernoulliCrossEntropyCost(x_tilde, images))
         params = get_params_dict()
-
-        enc_params = [params[k] for k in params.keys() if "enc" in k]
-        dec_params = [params[k] for k in params.keys() if "dec" in k]
-        emb_params = [params[k] for k in params.keys() if "embed" in k]
-
-        dec_grads = list(zip(tf.gradients(loss, dec_params), dec_params))
-        # scaled loss by alpha, but crank up vq loss grad
-        # like having a higher lr only on embeds
-        embed_grads = list(zip(tf.gradients(vq_loss, emb_params), emb_params))
-        grad_z = tf.gradients(rec_loss, z_q_x)
-        enc_grads = [(tf.gradients(z_e_x, p, grad_z)[0] + tf.gradients(beta * commit_loss, p)[0], p) for p in enc_params]
+        grads = tf.gradients(loss, params.values())
 
         learning_rate = 0.0002
         optimizer = tf.train.AdamOptimizer(learning_rate, use_locking=True)
-        train_step = optimizer.apply_gradients(dec_grads + enc_grads + embed_grads)
+        assert len(grads) == len(params)
+        j = [(g, p) for g, p in zip(grads, params.values())]
+        train_step = optimizer.apply_gradients(j)
 
     things_names = ["images",
-                    "bn_flag",
                     "x_tilde",
-                    "z_e_x",
-                    "z_q_x",
-                    "z_i_x",
-                    "z_emb",
                     "loss",
-                    "rec_loss",
                     "train_step"]
-    things_tf = [images,
-                 bn_flag,
-                 x_tilde,
-                 z_e_x,
-                 z_q_x,
-                 z_i_x,
-                 z_emb,
-                 loss,
-                 rec_loss,
-                 train_step]
-    assert len(things_names) == len(things_tf)
+    things_tf = [eval(name) for name in things_names]
     for tn, tt in zip(things_names, things_tf):
         graph.add_to_collection(tn, tt)
     train_model = namedtuple('Model', things_names)(*things_tf)
@@ -118,17 +89,14 @@ g, vs = create_graph()
 def loop(sess, itr, extras, stateful_args):
     x, = itr.next_batch()
     if extras["train"]:
-        feed = {vs.images: x,
-                vs.bn_flag: 0.}
-        outs = [vs.rec_loss, vs.loss, vs.train_step]
+        feed = {vs.images: x}
+        outs = [vs.loss, vs.train_step]
         r = sess.run(outs, feed_dict=feed)
         l = r[0]
-        t_l = r[1]
-        step = r[2]
+        step = r[1]
     else:
-        feed = {vs.images: x,
-                vs.bn_flag: 1.}
-        outs = [vs.rec_loss]
+        feed = {vs.images: x}
+        outs = [vs.loss]
         r = sess.run(outs, feed_dict=feed)
         l = r[0]
     return l, None, stateful_args
