4b1ab070baa7084fcef9d9b11375b54b07fde530
Kyle Kastner <kastnerkyle@gmail.com>
2018-05-04 12:44:43 -0400

    added music plotting / saving / loading utils

--- tfbldr/datasets/__init__.py
+++ tfbldr/datasets/__init__.py
@@ -12,3 +12,10 @@ from .iterators import tbptt_file_list_iterator
 from .iterators import char_textfile_iterator
 from .audio import soundsc
 from .audio import stft
+from .music import pitch_and_duration_to_piano_roll
+from .music import pitches_and_durations_to_pretty_midi
+from .music import quantized_to_pretty_midi
+from .music import plot_pitches_and_durations
+from .music import music21_to_pitch_duration
+from .music import music21_to_piano_roll
+from .music import plot_piano_roll
---
+++ tfbldr/datasets/music/__init__.py
@@ -0,0 +1,7 @@
+from .music import pitch_and_duration_to_piano_roll
+from .music import pitches_and_durations_to_pretty_midi
+from .music import quantized_to_pretty_midi
+from .music import plot_pitches_and_durations
+from .music import music21_to_pitch_duration
+from .music import music21_to_piano_roll
+from .music import plot_piano_roll
